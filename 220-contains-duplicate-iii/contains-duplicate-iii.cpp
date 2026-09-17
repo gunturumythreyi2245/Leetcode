a349c4b7ec770a7f1102ceae518fb958c25bb93a
Mythreyi Gunturu <gunturumythreyi@gmail.com>
2026-09-17 19:21:20 +0530

    Time: 178 ms (37.60%) | Memory: 157.3 MB (14.62%) - LeetSync

---
+++ 220-contains-duplicate-iii/contains-duplicate-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+        set<long> window;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > indexDiff) {
+                window.erase(nums[i - indexDiff - 1]);
+            }
+            auto it = window.lower_bound((long)nums[i] - valueDiff);
+            if (it != window.end() && *it <= (long)nums[i] + valueDiff) {
+                return true;
+            } 
+            window.insert(nums[i]);
+        }
+        return false;
+    }
+};
