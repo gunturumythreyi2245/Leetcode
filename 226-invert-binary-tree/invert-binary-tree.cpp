ebc963c94645cb3dc1c0ca80972f1228b9cd089e
Mythreyi Gunturu <gunturumythreyi@gmail.com>
2026-09-21 13:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (25.91%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        
+        std::swap(root->left, root->right);
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        return root;
+    }
+};
